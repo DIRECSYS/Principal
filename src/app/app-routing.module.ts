77a43bb399dfa4d249e186e27918686de4c73753
airamdev11 <airamdev11@outlook.com>
2022-08-05 18:26:18 -0500

    DIRECSYS-42 Ajustes de diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import ('./components/tabs/tabs.module').then(m => m.TabsPageModule)
   },
+  {
+    path: 'admin',
+    loadChildren: () => import ('./pages/admin/admin.module').then(m => m.AdminPageModule)
+  },
 
 ];
 
