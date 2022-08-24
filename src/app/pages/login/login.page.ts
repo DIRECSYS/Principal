c6a8d53243111de820e88b1bd4f72cae42070df9
airamdev11 <airamdev11@outlook.com>
2022-08-24 16:51:06 -0500

    Se descarta lo de autenticación, está por trabajarse...

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AuthService } from 'src/app/services/auth.service';
-import { ReportsService } from 'src/app/services/reports.service';
-import { Validators, FormBuilder,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,54 +6,27 @@ import { Validators, FormBuilder,FormGroup } from '@angular/forms';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  public sesionIniciada: boolean;
-  public usuario: any;
 
-  items2: any;
-  ngForm : FormGroup;
+  public correo: any;
 
   
-  constructor(private rs: ReportsService, private _authService: AuthService ,public formBuilder: FormBuilder) {
+  constructor() {
 
   }
 
   ngOnInit(): void {
 
-    this.ngForm = this.formBuilder.group({
-      user: ['', [Validators.required, Validators.minLength(2)]],
-   })
-
-    
-    this.usuarioLoggeado()
-  }
-
-  usuarioLoggeado(){
-    this._authService.getInfoUsuarioLoggeado().subscribe(userData=>{
-      if(userData != null){
-        this.sesionIniciada = true;
-        this.usuario = userData;
-      }
-      else{
-        this.sesionIniciada = false;
-      }
-    })
   }
 
   login(){
 
-    let usuario = this.usuario;
+    let correo = this.correo;
 
-    if (usuario == "admin"){
+    if (correo == "admin"){
         location.href = "/admin-tabs";
       }
-      else if ( usuario == "user"){
+      else if ( correo == "user"){
         location.href = "/tabs"
       }
-  }
-
-  logOut(): void{
-    this._authService.logOut().then(userData => {
-      this.sesionIniciada = false;
-    })
   }
 }
